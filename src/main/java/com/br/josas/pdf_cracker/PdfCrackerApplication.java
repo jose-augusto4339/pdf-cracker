f31311c999658be11922f2803821644555cff138
Jose Augusto <joseaugustoribeiro1998@gmail.com>
2026-03-21 15:44:05 -0300

    primeiro commit

---
+++ src/main/java/com/br/josas/pdf_cracker/PdfCrackerApplication.java
@@ -0,0 +1,13 @@
+package com.br.josas.pdf_cracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfCrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfCrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/josas/pdf_cracker/PdfCrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.josas.pdf_cracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfCrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
